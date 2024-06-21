bc9f6c44e88f515ac95b238426a526dc34122cf7
Matheus Barbosa <barbosamatheus035@gmail.com>
2024-06-20 22:32:58 -0300

    Update Ex2.py

--- Aplicações Práticas de POO/Ex2.py
+++ Aplicações Práticas de POO/Ex2.py
@@ -6,7 +6,7 @@ class Produto:
         self.preco=preco
     
     def __str__(self):
-        return f'Nome: {self.nome}\nCodigo: {self.codigo}\nQuantidade: {self.quantidade}'
+        return f'Nome: {self.nome}  | Preço R$: {self.preco}  |  Quantidade: {self.quantidade} | Codigo: {self.codigo}'
 
 class Fornecedor:
     def __init__(self,nome,contato):
@@ -22,7 +22,8 @@ class Estoque:
     
     def adicionar(self,produto,quantidade):
         self.lista_produtos.append(produto)
-        self.lista_produtos.append(quantidade)
+        produto.quantidade=quantidade
+        self.lista_produtos.append(produto.quantidade)
         print(f'{produto} adicionado com sucesso a lista!')
     
     def remover(self,produto):
@@ -45,7 +46,7 @@ class Estoque:
         print(f'Produto com o codigo: {codigo} não encontrado no sistema!')
         
     def calcular(self):
-        return (produto.preco * produto.quantido for produto in self.lista_produtos)
+        return produto.preco * produto.quantidade 
     
     def __str__(self):
         produto_pedido = '\n'.join(str(produto) for produto in self.lista_produtos)
@@ -58,21 +59,21 @@ print('Bem Vindo ao Key Market')
 print('-'*40)
 
 nome=input('Ola Para começar insira seu nome:\n-->>')
-contato=input(f'Certo {nome} agora insira um email para contato')
+contato=input(f'Certo {nome} agora insira um email para contato:\n-->>')
 fornecedor=Fornecedor(nome,contato)
-
+estoque=Estoque(fornecedor)
 produtos = [
-    Produto('Mouse',123, 100 , 5.99),
-    Produto('Teclado',124, 80, 10.99),
-    Produto('Monitor',125,50, 15.99),
-    Produto('Gabinete',126,75, 7.99),
-    Produto('Mouse Pad',127,100, 3.99)
+    Produto('Mouse    ', 123 , 100  , 5.99),
+    Produto('Teclado  ', 124 ,  80  , 8.99),
+    Produto('Monitor  ', 125 ,  50  , 9.99),
+    Produto('Gabinete ', 126 ,  75  , 7.99),
+    Produto('Mouse Pad', 127 , 100  , 3.99)
 ]
 
 print(f'Ola {nome} aqui esta a lista de produtos que temos em nosso estoque')
 while True:
     while True:
-        for i,produto in produto:
+        for i, produto in enumerate(produtos, start=1):
             print(f'{i}.{produto}')
         
         escolha=input('Digite o numero do item que deseja comprar ou "sair" para finalizar o pedido:\n-->>')
@@ -82,4 +83,15 @@ while True:
             break
         
         try:
+            escolha_num = int(escolha)
+            if 1 <= escolha_num <= len(produtos):
+                estoque.adicionar(produtos[escolha_num - 1],quantidade)
+                print('-'*40)    
+            else:
+                print(f'Escolha inválida, por favor tente novamente.')
+        except ValueError:
+            print(f'Entrada inválida, por favor insira um número ou "sair".')
             
+        print('\nPedido:')
+        print(estoque)
+        
