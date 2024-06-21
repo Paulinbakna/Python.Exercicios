26379b28cc1f3e0307bf0de0c9f5ee8a088cb9f6
Matheus Barbosa <barbosamatheus035@gmail.com>
2024-06-21 18:00:46 -0300

    Update Ex3.py

--- Aplicações Práticas de POO/Ex3.py
+++ Aplicações Práticas de POO/Ex3.py
@@ -44,4 +44,46 @@ class RegistroPresenca:
         if turma in self.registros and data in self.registros[turma]:
             return f'Registro de presença para a turma de {turma.disciplina} na data {data}:\n' + '\n'.join(str(aluno) for aluno in turma.alunos if aluno.matricula in self.registros[turma][data])
         return f'Nenhum registro de presença encontrado para a turma de {turma.disciplina} na data {data}'
-    
+
+
+print('-'*40)
+men='Colegio Python'
+men.center(40)
+print(men)
+print('-'*40)
+alunos = [
+        Aluno('João Silva','123'),
+        Aluno('Maria Oliveira','124'),
+        Aluno('CArlos pereira','125'),
+        Aluno('Ana Grabieli','126'),
+        Aluno('Marcos Vinicios','127')
+    ]
+
+turmas = [
+        Turma('Matematica'),
+        Turma('História'),
+        Aluno('Português'),
+        Aluno('Ingles'),
+        Aluno('Biologia')
+    ]
+
+while True:
+    while True:
+        for i,aluno in enumerate(alunos,start=1):
+            print(f'{i}.{aluno}')
+        print('-'*40)
+        
+        escolha=input('Digite o numero do aluno:\n-->>')
+        print('Certo o que deseja fazer agora?')
+        print('[ 1 ] Adicionar a uma turma')    
+        print('[ 2 ] Remover de uma turma')    
+        print('[ 3 ] Registrar presença')
+        print('[ 4 ] Exibir Registro')
+        n=input('-->>')
+        if n == '1':
+            escolha_num = int(escolha)
+            if 1 <= escolha_num <= len(alunos):
+                alunos.adicionar(alunos[escolha_num - 1])
+                print('-'*40)    
+        
+                
