a4a57085c1d2bb1cb67cb21ef784fe956f68f37b
Matheus Barbosa <barbosamatheus035@gmail.com>
2024-06-26 19:35:05 -0300

    .

--- Praticando/Ex27.py
+++ Praticando/Ex27.py
@@ -4,66 +4,100 @@ class Produto:
         self.preco=preco
     
     def __str__(self):
-        return f'Nome do produto: {self.nome} | Preço do Produto R$: {self.preco}'
+        return f'Nome : {self.nome} | Preço R$: {self.preco}'
 
 class Estoque:
     def __init__(self):
         self.estoque=[]
         self.carrinho=[]
     
-    def venda(self,produto):
-        for produto in self.estoque:
-            self.estoque.remove(produto)
+    def adicionar_carinho(self,produto):
+        if  produto in self.estoque:
+            print(f'{produto} adicinado com sucesso ao carrinho!')
             self.carrinho.append(produto)
-            print(f'Produto adicinado com sucesso ao carrinho!')
         else:
             return f'{produto} não encontrado !'
         
     def remover_carinho(self,produto):
-        self.carrinho.remove(produto)
-        print(f'{produto} removido do carrinho!')
-    
-    def itens_no_carirnho(self):
-        for item in self.carrinho:
-            print(item)
+        if produto in self.carrinho:
+            self.carrinho.remove(produto)
+            print(f'{produto} removido do carrinho!')
+        else:
+            print(f'{produto} não está no carrinho!')
 
+    def itens_no_carirnho(self):
+        if self.carrinho:
+            for item in self.carrinho:
+                print(item)
+        else:
+            print('O carrinho esta vazio.')
+            
+    def adicionar_produto(self,produto):
+        self.estoque.append(produto)
+        
+    def remover_estoque(self,produto):
+        if produto in self.estoque:
+            self.estoque.remove(produto)
+            print(F'{Produto} removido do estoque!')
+        else:
+            print(f'{produto} não está no estoque!')
+            
     def mostrar_estoque(self):
-        for produto in self.estoque:
-            print(produto)
+        for p, produto in enumerate(self.estoque,start=1):
+            print(f'{p}.{produto}')
     
 estoque=Estoque()        
 produtos = [
-        Produto('Mouse    ', 100),
-        Produto('Teclado  ', 200),
-        Produto('Celular  ', 1250),
-        Produto('Tablet', 3260 ),
-        Produto('Nootebok', 7540),
-        Produto('Computador', 8960)
+        Produto('Mouse       ', 100),
+        Produto('Teclado     ', 200),
+        Produto('Celular     ', 1250),
+        Produto('Tablet      ', 3260),
+        Produto('Nootebok    ', 7540),
+        Produto('Computador  ', 8960)
     ]
 for produto  in produtos:
     estoque.adicionar_produto(produto)
+    
 print('Bem vindo a Loja!')
 print('Aqui esta a lista de produtos em nosso estoque')
 
 while True:
-    try:
-        estoque.mostrar_estoque()
-        escolha=('Digite o numero do item que você deseja adicionar ao carinho')
+    while True:
         
+        estoque.mostrar_estoque()
+        print(f'"Digite sair para finalizar"')
+        escolha=input('Digite o numero do item que você deseja adicionar ao carinho : ')
+            
         if escolha.lower() == 'sair':
             break
+        try:    
+            escolha_num=int(escolha)
+            if 1 <= escolha_num <=len(produtos):
+                estoque.adicionar_carinho(estoque.estoque[escolha_num-1])
         
-        escolha_num=int(escolha)
-        if escolha_num < 0 or escolha_num  >= len(produtos):
-            estoque.venda(produtos[escolha_num-1])
-    
-        else:
-            print(f'Escolha invalida,por favor tente novamente!')
-    except ValueError:
-        print(f'Entrada invalida,por favor insira um número ou "sair"')
+            else:
+                print(f'Escolha invalida,por favor tente novamente!')
+        except ValueError:
+            print(f'Entrada invalida,por favor insira um número ou "sair"')
+        
     while True:
-        print(f'Certo aq esta os itens do carrinho:')
+        print(f'Certo aqui esta os itens no seu carrinho:')
         estoque.itens_no_carirnho()
         resp=input('Gostaria de remover algum item do carrinho?[S/N]')
-            
+        if resp.lower() == 'n':
+            break
+        elif resp.lower() == 's':
+            nome_produto=input('Certo digite o nome do item que deseja remover:')
+            produto_para_remover=None
+            for produto in estoque.carrinho:
+                produto_para_remover= produto
+                break
+            if produto_para_remover:
+                estoque.remover_carinho(produto_para_remover)
+            else:
+                print(f'{nome_produto} não encontrado no carrinho')
+    perg=input('Deseja continuar comprando?[S/N]')
+    if perg.lower() in 'Nn':
+        break
+    
         
