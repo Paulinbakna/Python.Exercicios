1c0c963ca72a1dabcde8fef1c9eadd46c6ce6aae
Matheus Barbosa <barbosamatheus035@gmail.com>
2024-06-27 22:51:04 -0300

    Update Ex28.py

--- Praticando/Ex28.py
+++ Praticando/Ex28.py
@@ -11,12 +11,8 @@ class Quarto:
         self.quartos=[]
         self.quartos_reservados=[]
     def adicionar_quarto(self,quarto):
-        if quarto in self.quartos:
-            self.quartos.append(quarto)
-            print(f'Quarto N° {quarto.numero_quarto} adicionado a lista de reservas!')
-        else:
-            print(f'Quarto N° {quarto} não foi encontrado na lista de reservas!')
-        
+        self.quartos.append(quarto)
+                
     def remover_quarto(self,quarto):
         if quarto in self.quartos:
             self.quartos.remove(quarto)
@@ -36,4 +32,70 @@ class Quarto:
             self.quartos_reservados.remove(quarto)
             self.quartos.append(quarto)
             print(f'O Quarto N° {quarto} não está mais reservado!\n e foi adicionado a lsita de reservas!')
-        
+    
+    def mostrar_quartos(self):
+        for q,quarto in enumerate(self.quartos,start=1):
+            print(f'{q}.{quarto}')
+    
+    
+    def mostrar_reservados(self):
+        for q,reservado in enumerate(self.quartos_reservados,start=1):
+            print(f'{q}.{reservado}')
+    
+class Cliente:
+    def __init__(self,nome,email):
+        self.nome=nome
+        self.email=email
+    def __str__(self):
+        return f'Nome: {self.nome} | Contato: {self.email}'
+    
+quarto=Quarto()
+
+quartos = [
+        Hotel('456',160),
+        Hotel('854',160),
+        Hotel('954',160),
+        Hotel('524',160),
+        Hotel('426',160)
+    ]
+for q in quartos:
+    quarto.adicionar_quarto(q)
+    
+print('-'*40)
+print('Bem Vindo ao Hotel Python')
+print('-'*40)
+nome=input('Antes de entrar digite seu nome:')
+contato=input(f'Certo {nome} digite um email para contato: ')
+cliente=Cliente(nome,contato)
+print(f'Aq esta a lista de quartos disponiveis para reserva: ')
+quarto.mostrar_quartos()
+while True:
+    while True:
+        print("Digite sair para encerar o atendimento")
+        escolha=input('Digite o numero  do quarto de deseja reservar:')
+        if escolha.lower() == 'sair':
+            break    
+
+        escolha_num=int(escolha)
+        if 1 <= escolha_num <= len(quartos):
+            quarto.reservar_quarto(escolha[escolha_num-1])
+    while True:
+        quarto.mostrar_reservados()
+        perg=input(f'Certo {nome} gostaria de remover algum quarto?[S/N]')
+        if perg.lower() == 'n':
+            break
+        elif perg.lower() == 's':
+            quarto_remover=input('Certo qual quarto deseja remover?')
+            quarto_para_remover=None
+            for quarto in quarto.quartos_reservados:
+                quarto_para_remover=quarto
+                break
+            if quarto_para_remover:
+                quarto.liberar_quarto(quarto_para_remover)
+            else:
+                print(f'Quarto não encontrado nos quartos reservados')
+    print(cliente)
+    quarto.mostrar_reservados()
+    resp=input('Gostaria de reservar mais algum quarto?=[S/N]')
+    if resp.lower() == 'n':
+        break
