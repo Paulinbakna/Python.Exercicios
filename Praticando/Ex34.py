c21dc9507e708e6167daa36eb79ddd4cd2000eb4
Matheus Barbosa <barbosamatheus035@gmail.com>
2024-07-04 22:20:17 -0300

    .

---
+++ Praticando/Ex34.py
@@ -0,0 +1,34 @@
+import json
+
+def adicionar_par_chave(caminho,chave,valor):
+    with open(caminho,'r',encoding='utf-8') as arquivo:
+        dados=json.load(arquivo)
+        
+    dados[chave]=valor
+    
+    with open(caminho,'w',encoding='utf-8') as arquivo:
+        json.dump(dados,arquivo,ensure_ascii=False,indent=4)
+
+
+dados_exemplo={
+    "nome":"João",
+    "idade":25,
+    "cidade":"São Paulo",
+    "habilidades":["Python","Css","C++","JavaScript"]
+}
+
+caminho=r'C:\Users\Paulin\Documents\GitHub\Python.Exercicios\Praticando\arquivo_teste.json'
+
+with open(caminho,'w',encoding='utf-8')as arquivo:
+    json.dump(dados_exemplo,arquivo,ensure_ascii=False,indent=4)
+
+chave='experiencia'
+valor='Pleno'
+adicionar_par_chave(caminho,chave,valor)
+print(f'A chave "{chave}" com valor "{valor}" foi adicionado ao arquivo "{caminho}"')
+
+with open(caminho,'r',encoding='utf-8') as arquivo:
+    valores=json.load(arquivo)
+    print('Conteudo atualizado do arquivo Json')
+    print(json.dumps(valores,ensure_ascii=False,indent=4))
+    
