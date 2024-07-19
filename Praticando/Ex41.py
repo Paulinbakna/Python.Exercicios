8dbd752ea0249186f69b27a9a7f4f2ffd8074887
Matheus Barbosa <barbosamatheus035@gmail.com>
2024-07-18 22:51:42 -0300

    Update Ex41.py

--- Praticando/Ex41.py
+++ Praticando/Ex41.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+from datetime import datetime,time
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 #Verifique se uma string de entrada represenda uma data válida no formato yyyy-mm-dd
 
 dia=int(input('Digite a data (dd): '))
@@ -13,4 +14,22 @@ try:
     datetime.strptime(data_completa,'%d/%m/%Y')
     print(f'Data: {data_completa}')
 except ValueError:
-    print(f'Data no formato incorreto!')
+    print('Data no formato incorreto!')
+
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#Verifique se uma string de entrada representa uma hora valida no formato hh:mm:ss
+
+hora=int(input('Digite o numero da hora:'))
+minuto=int(input('Digite o minuto: '))
+segundos=int(input('Digite os segundos:'))
+print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+try:
+    hora_digitada=time(hour=hora,minute=minuto,second=segundos)
+    hora_completa=hora_digitada.strftime('%H:%M:%S')
+    datetime.strptime(hora_completa,'%H:%M:%S')
+    print(f'Hora: {hora_completa}')
+except ValueError:
+    print('Hora digitada Incorreta!')
+    
